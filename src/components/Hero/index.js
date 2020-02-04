c52b8e50b434d80aa3cf6d5fd0ed2bc373150f81
Anghy <anghyf27@hotmail.com>
2020-02-03 19:05:32 -0500

    included styles for the web

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,37 +1,76 @@
 import React, { Component } from 'react'
 import Carousel from 'nuka-carousel'
+import Media from 'react-media'
+import { breakpoints } from '../../styles'
 // import styled from 'styled-components'
 
 class Hero extends Component {
   render() {
     return (
       <>
-        <Carousel withoutControls autoplay={true}>
-          <img
-            src='https://res.cloudinary.com/apomarn/image/upload/v1580715339/fortified/hero.png'
-            width='100%'
-            height='896px'
-            alt='hero1'
-          />
-          <img
-            src='https://res.cloudinary.com/apomarn/image/upload/v1578420177/abnb/Imageapt1_qzynuw.png'
-            width='100%'
-            height='896px'
-            alt='hero2'
-          />
-          <img
-            src='https://res.cloudinary.com/apomarn/image/upload/v1559144034/WeDo/guest-list-container.jpg'
-            width='100%'
-            height='896px'
-            alt='hero3'
-          />
-          <img
-            src='https://res.cloudinary.com/apomarn/image/upload/v1559144034/WeDo/share-container.jpg'
-            width='100%'
-            height='896px'
-            alt='hero4'
-          />
-        </Carousel>
+        <Media query={`(max-width: ${breakpoints.desktop}px)`}>
+          {matches =>
+            matches ? (
+              <>
+                <Carousel withoutControls autoplay={true}>
+                  <img
+                    src='https://res.cloudinary.com/apomarn/image/upload/v1580715339/fortified/hero.png'
+                    width='100%'
+                    height='896px'
+                    alt='hero1'
+                  />
+                  <img
+                    src='https://res.cloudinary.com/apomarn/image/upload/v1578420177/abnb/Imageapt1_qzynuw.png'
+                    width='100%'
+                    height='896px'
+                    alt='hero2'
+                  />
+                  <img
+                    src='https://res.cloudinary.com/apomarn/image/upload/v1559144034/WeDo/guest-list-container.jpg'
+                    width='100%'
+                    height='896px'
+                    alt='hero3'
+                  />
+                  <img
+                    src='https://res.cloudinary.com/apomarn/image/upload/v1559144034/WeDo/share-container.jpg'
+                    width='100%'
+                    height='896px'
+                    alt='hero4'
+                  />
+                </Carousel>
+              </>
+            ) : (
+              <>
+                <Carousel withoutControls autoplay={true}>
+                  <img
+                    src='https://res.cloudinary.com/apomarn/image/upload/v1580715339/fortified/hero.png'
+                    width='100%'
+                    height='896px'
+                    alt='hero1'
+                  />
+                  <img
+                    src='https://res.cloudinary.com/apomarn/image/upload/v1578420177/abnb/Imageapt1_qzynuw.png'
+                    width='100%'
+                    height='896px'
+                    alt='hero2'
+                  />
+                  <img
+                    src='https://res.cloudinary.com/apomarn/image/upload/v1559144034/WeDo/guest-list-container.jpg'
+                    width='100%'
+                    height='896px'
+                    alt='hero3'
+                  />
+                  <img
+                    src='https://res.cloudinary.com/apomarn/image/upload/v1559144034/WeDo/share-container.jpg'
+                    width='100%'
+                    height='896px'
+                    alt='hero4'
+                  />
+                </Carousel>
+              </>
+            )
+          }
+        </Media>
       </>
     )
   }
---
+++ src/styles.js
@@ -0,0 +1,82 @@
+const montserratFamily = `
+    font-family: Montserrat;
+`
+
+const h1Montserrat = `
+    font-size: 48px;
+    ${montserratFamily};
+`
+
+const h2Montserrat = `
+    font-size: 32px;
+    ${montserratFamily}
+`
+
+const h3Montserrat = `
+    font-size: 16px;
+    ${montserratFamily}
+`
+
+const h4Montserrat = `
+    font-size: 14px;
+    ${montserratFamily}
+`
+
+const h5Montserrat = `
+    font-size: 10px;
+    ${montserratFamily}
+`
+
+const dancingScriptFamily = `
+    font-family: Dancing Script;
+`
+
+const h1DancingScript = `
+    font-size: 48px;
+    ${dancingScriptFamily};
+`
+
+const h2DancingScript = `
+    font-size: 32px;
+    ${dancingScriptFamily}
+`
+
+const h3DancingScript = `
+    font-size: 16px;
+    ${dancingScriptFamily}
+`
+
+const h4DancingScript = `
+    font-size: 14px;
+    ${dancingScriptFamily}
+`
+
+const h5DancingScript = `
+    font-size: 10px;
+    ${dancingScriptFamily}
+`
+
+export const textStyles = {
+  h1Montserrat,
+  h2Montserrat,
+  h3Montserrat,
+  h4Montserrat,
+  h5Montserrat,
+  h1DancingScript,
+  h2DancingScript,
+  h3DancingScript,
+  h4DancingScript,
+  h5DancingScript
+}
+
+export const colors = {
+  brown: '#956140',
+  lightBrown: '#BC7E4F',
+  black: '#000000',
+  white: '#ffffff',
+  tan: '#F3BF98'
+}
+
+export const breakpoints = {
+  desktop: 768
+}
