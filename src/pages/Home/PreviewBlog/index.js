e79eb711f3b7daff11ec7e9e424e882106504a78
Anghy <anghyf27@hotmail.com>
2020-02-04 18:57:31 -0500

    previewblog css ready

--- src/pages/Home/PreviewBlog/index.js
+++ src/pages/Home/PreviewBlog/index.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { StyledDivider } from '../../../components/Anchors'
 import { colors, textStyles, breakpoints } from '../../../styles'
 
 const MainContainer = styled.div`
-  width: 100%
   display: flex;
+  flex-direction: column;
+
+  margin-top: 85px;
+`
+const Header = styled.p`
+  ${textStyles.h2DancingScript}
+  color: ${colors.brown};
+  font-weight: 700;
+  align-self: flex-start;
+  margin-left: 45px;
+`
+const BlogsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Title = styled.p`
+  font-size: 16px;
+  color: ${colors.lightBrown};
+  margin-bottom: 10px;
+  font-weight: 900;
+  padding-left: 45px;
+`
+
+const Paragraph = styled.p`
+  ${textStyles.h4Montserrat}
+  color: ${colors.black};
+  text-align: justify;
+  text-justify: inter-word;
+  padding-left: 45px;
+  padding-right: 45px;
+  height: 60px;
+`
+const AllBlogs = styled.p`
+  font-size: 16px;
+  color: ${colors.brown};
+  font-weight: 900;
+  text-align: right;
+  padding-right: 45px;
+  text-decoration: underline;
+  margin-bottom: 85px;
 `
 
 class PreviewBlog extends Component {
   render() {
     return (
       <MainContainer>
-        <p>Recent Blogs</p>
-        <div>
-          <p>Titulo</p>
-          <p>Parrafo kuysrgaruygfawlirufgwleiufg fkreufgw fweufgwef ukrwyfvawr </p>
-        </div>
-        <div>
-          <p>Titulo</p>
-          <p>Parrafo kuysrgaruygfawlirufgwleiufg fkreufgw fweufgwef ukrwyfvawr </p>
-        </div>
+        <Header>Recent Blogs...</Header>
+        <BlogsContainer>
+          <div>
+            <Title>Titulo</Title>
+            <Paragraph>Parrafo kuy srgar uyg fawl iruf wleiufg fkre ufgw fweufg wef ukrwy fvawr </Paragraph>
+          </div>
+          <div>
+            <Title>Titulo</Title>
+            <Paragraph>Parrafo kuysrgaruygfawlirufgwleiufg fkreufgw fweufgwef ukrwyfvawr </Paragraph>
+          </div>
+        </BlogsContainer>
+        <AllBlogs>All Blogs</AllBlogs>
+        <StyledDivider />
       </MainContainer>
     )
   }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Hero from '../../components/Hero'
 import About from './About'
+import PreviewBlog from './PreviewBlog'
 
 class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends Component {
       <>
         <Hero />
         <About />
+        <PreviewBlog />
       </>
     )
   }
