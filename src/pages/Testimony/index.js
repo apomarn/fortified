a3ffc7735275b63972d93219bb705e70240a5ffe
Anghy <anghyf27@hotmail.com>
2020-02-10 01:14:56 -0500

    testimony component html ready

--- src/pages/Testimony/index.js
+++ src/pages/Testimony/index.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
 
 class index extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blog: {}
+    }
+  }
   render() {
     return (
       <div>
-        <p>Testimonyyyyy</p>
+        <img src={this.state.blog.user_image} alt='testimoni-profile-pic' />
+        <p>{this.state.blog.user_name}</p>
+        <p>{this.state.blog.testimony}</p>
       </div>
     )
+  }
+  componentDidMount() {
+    var url
+    if (process.env.NODE_ENV === 'development') {
+      url = 'http://localhost:5000'
+    }
+    fetch(`${url}/alltestimonies/${this.props.match.params.title}`)
+      .then(data => data.json())
+      .then(testimonyData => {
+        this.setState({ blog: testimonyData[0] })
+      })
+      .catch(err => console.log(err))
   }
 }
 
