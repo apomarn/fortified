085472001c6366a52222164d2b1000e570d65d49
Anghy <anghyf27@hotmail.com>
2020-02-21 16:04:00 -0500

    separated css from backdrop

--- src/components/Navbar/Backdrop/index.js
+++ src/components/Navbar/Backdrop/index.js
@@ -1,15 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const MainContainer = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  background: rgba(0, 0, 0, 0.5);
-  z-index: 100;
-`
+import { MainContainer } from './style'
 
 const Backdrop = props => <MainContainer onClick={props.click} />
 export default Backdrop
