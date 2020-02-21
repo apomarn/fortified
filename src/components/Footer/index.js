4e363b870eda1c4639aca1bc5ff3b004debf874b
Anghy <anghyf27@hotmail.com>
2020-02-21 16:01:55 -0500

    separated css in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,34 +1,6 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import { textStyles, colors } from '../../styles'
+import { MainContainer, ContactContainer, ImageContact, BuilderInfo } from './style'
 
-const MainContainer = styled.div`
-  background-color: ${colors.black};
-  position: absolute;
-  bottom: 1;
-  left: 0;
-  right: 0;
-`
-
-const ContactContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-  padding-top: 25px;
-  margin-bottom: 25px;
-  width: 100%;
-`
-const ImageContact = styled.img`
-  width: 50px;
-  height: 50px;
-`
-const BuilderInfo = styled.p`
-  ${textStyles.h5Montserrat}
-  color: ${colors.white};
-  text-align: right;
-    padding-right: 15px;
-    padding-bottom: 10px;
-    margin: 0;
-`
 class Footer extends Component {
   render() {
     return (
---
+++ src/components/Footer/style.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import { textStyles, colors } from '../../styles'
+
+export const MainContainer = styled.div`
+  background-color: ${colors.black};
+  position: absolute;
+  bottom: 1;
+  left: 0;
+  right: 0;
+`
+
+export const ContactContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding-top: 25px;
+  margin-bottom: 25px;
+  width: 100%;
+`
+export const ImageContact = styled.img`
+  width: 50px;
+  height: 50px;
+`
+export const BuilderInfo = styled.p`
+  ${textStyles.h5Montserrat}
+  color: ${colors.white};
+  text-align: right;
+    padding-right: 15px;
+    padding-bottom: 10px;
+    margin: 0;
+`
