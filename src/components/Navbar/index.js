a4902110ef037e2b467a8648f1fcf40fa02f75b5
Anghy <anghyf27@hotmail.com>
2020-02-21 16:10:36 -0500

    separated css from Navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,51 +1,10 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
 import Media from 'react-media'
-// import { MainContainer, PageLogo } from './style'
-import { breakpoints, textStyles, colors } from '../../styles'
-import { Link } from 'react-router-dom'
+import { breakpoints } from '../../styles'
+import { MainContainer, PageLogo, Links } from './style'
 import SideDrawer from './Sidebar/sideDrawer'
 import Backdrop from './Backdrop'
 import ButtonLogo from './ButtonLogo'
-
-export const MainContainer = styled.div`
-  width: 100%;
-  position: absolute;
-
-  top: 0px;
-  left: 0px;
-  padding-left: 20px;
-  padding-top: 10px;
-  @media screen and (min-width: ${breakpoints.desktop}px) {
-    position: absolute;
-    width: 100%;
-    top: 0px;
-    left: 0px;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    background-color: rgb(250, 250, 250, 40%);
-  }
-`
-
-export const PageLogo = styled.img`
-  width: 60px;
-  heigth: 60px;
-  @media screen and (min-width: ${breakpoints.desktop}px) {
-    width: 100px;
-    heigth: 100px;
-  }
-`
-const Links = styled(Link)`
-  ${textStyles.h3Montserrat}
-  color: white;
-  font-weight: 700;
-  :hover {
-    cursor: pointer;
-    color: ${colors.brown};
-    text-decoration: none;
-  }
-`
 
 class Navbar extends Component {
   state = {
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -1,25 +1,42 @@
 import styled from 'styled-components'
-import { breakpoints } from '../../styles'
+import { breakpoints, textStyles, colors } from '../../styles'
+import { Link } from 'react-router-dom'
 
-// export const MainContainer = styled.div`
-//   position: absolute;
-//   width: auto;
-//   top: 25px;
-//   left: 25px;
-//   display: flex;
-//   justity-content: space-between;
-//   @media screen and (min-width: ${breakpoints.desktop}px) {
-//     position: absolute;
-//     top: 40px;
-//     left: 40px;
-//   }
-// `
+export const MainContainer = styled.div`
+  width: 100%;
+  position: absolute;
 
-// export const PageLogo = styled.img`
-//   width: 60px;
-//   heigth: 60px;
-//   @media screen and (min-width: ${breakpoints.desktop}px) {
-//     width: 100px;
-//     heigth: 100px;
-//   }
-// `
+  top: 0px;
+  left: 0px;
+  padding-left: 20px;
+  padding-top: 10px;
+  @media screen and (min-width: ${breakpoints.desktop}px) {
+    position: absolute;
+    width: 100%;
+    top: 0px;
+    left: 0px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: rgb(250, 250, 250, 40%);
+  }
+`
+
+export const PageLogo = styled.img`
+  width: 60px;
+  heigth: 60px;
+  @media screen and (min-width: ${breakpoints.desktop}px) {
+    width: 100px;
+    heigth: 100px;
+  }
+`
+export const Links = styled(Link)`
+  ${textStyles.h3Montserrat}
+  color: white;
+  font-weight: 700;
+  :hover {
+    cursor: pointer;
+    color: ${colors.brown};
+    text-decoration: none;
+  }
+`
