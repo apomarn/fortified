95a27fe7ad4eb1e605bcd3f2efb455abad374b20
Anghy <anghyf27@hotmail.com>
2020-02-21 16:13:11 -0500

    separated css from EachBlog component

--- src/pages/AllBlogs/EachBlog/index.js
+++ src/pages/AllBlogs/EachBlog/index.js
@@ -1,44 +1,6 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import { textStyles, colors } from '../../../styles'
 import { StyledDivider } from '../../../components/Anchors'
-
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0 45px;
-`
-
-const Image = styled.img`
-  height: 300px;
-  width: 300px;
-  margin-top: 100px;
-`
-const Title = styled.p`
-  margin: 25px 0;
-  font-size: 16px;
-  font-weight: 700;
-  color: ${colors.lightBrown};
-`
-const Paragraph = styled.p`
-  ${textStyles.h4Montserrat}
-  width: 300px;
-  color: ${colors.white};
-  overflow: hidden;
-  white-space: wrap;
-  width: 100%;
-  height: 100px;
-`
-const ReadMore = styled(Link)`
-  font-size: 14px;
-  color: ${colors.lightBrown};
-  text-decoration: underline;
-  font-weight: 700;
-  align-self: flex-end;
-  padding: 30px 0 45px;
-`
+import { MainContainer, Image, Title, Paragraph, ReadMore } from './style'
 
 class EachBlog extends Component {
   render() {
