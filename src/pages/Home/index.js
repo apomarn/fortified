0057d41514deafc03625a46acb1453feed25893e
Anghy <anghyf27@hotmail.com>
2020-03-25 21:10:59 -0400

    modified home css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,12 +4,7 @@ import About from './About'
 import PreviewBlog from './PreviewBlog'
 import Testimonials from './Testimonials'
 import Footer from '../../components/Footer'
-import styled from 'styled-components'
-import { colors } from '../../styles'
-
-const MainContainer = styled.div`
-  background-color: ${colors.black};
-`
+import { MainContainer } from './style'
 
 class Home extends Component {
   render() {
